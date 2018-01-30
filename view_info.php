c3b90dbe03e34e390e702981f02a786fec019c41
shivapbhusal <civabhusal@gmail.com>
2018-01-29 21:40:29 -0500

    add view_info.php

---
+++ view_info.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>SQL Injection Example</title>
+      <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <div class="login-page">
+  <div class="form">
+
+<table style="width:100%">
+  <tr>
+    <th>Name:</th>
+    <td></td>
+  </tr>
+  <tr>
+    <th>Email:</th>
+    <td></td>
+  </tr>
+  <tr>
+    <th>Hometown:</th>
+    <td></td>
+  </tr>
+  <tr>
+    <th>Phone Number:</th>
+    <td></td>
+  </tr>
+</table>
+
+  </div>
+</div>
+</body>
+
+</html>
